494f3e891864e9d996e9f600b24a80c8abd166b2
abdulladone94 <abdulladone94@gmail.com>
2021-10-28 06:40:47 +0530

    Initial setup

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
 import express from "express";
 import ejs from "ejs";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/practiceDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("Home");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.userName,
+    password: req.body.password,
+  });
+  newUser.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
 
 app.listen(3001, () => {
   console.log("Started port 3001");
